d8c8299309d49898de771e00b220787e7fee52e7
pruthvirajkamble <20bcs104@iiitdwd.ac.in>
2024-03-09 22:33:20 +0530

    Datatypes in js
    Datatypes in js

---
+++ 02_typeof.js
@@ -0,0 +1,54 @@
+console.log("=================Question 1=================");
+console.log("=================Before initialization=================");
+var bankName;
+console.log("Declare variable :", bankName);
+var typeofBankName = typeof bankName;
+console.log("Declare data type:", typeofBankName);
+
+console.log("=================after initialization=================");
+bankName = "SBI Bank";
+console.log("Declare variable :", bankName);
+var typeofBankName = typeof bankName;
+console.log("Declare data type :", typeofBankName);
+
+console.log("=================Question 2=================");
+console.log("=================On the same line Declare initialization=================");
+var marks = '90%';
+console.log("Marks:",marks);
+var marksDataType = typeof marks;
+console.log("Marks Data Type:",marksDataType);
+
+console.log("=================Question 3=================");
+console.log("=================Before initialization=================");
+var isWorking;
+console.log("Is Working:", isWorking);
+var dataIsWorking = typeof isWorking;
+console.log("Data type :", dataIsWorking);
+console.log("=================after initialization=================");
+isWorking = true;
+console.log("Is Working:", isWorking);
+var dataIsWorking = typeof isWorking;
+console.log("Data type :", dataIsWorking);
+
+console.log("=================Question 4=================");
+console.log("=================Question 4-(A)=================");
+var totalCount = "Hundred and Seven";
+console.log("Total Count:",totalCount);
+var dataTotalCount = typeof totalCount;
+console.log("Data type of Total Count:", dataTotalCount);
+
+console.log("=================Question 4-(B)=================");
+var totalCount = 107;
+console.log("Updated value of total count:",totalCount);
+var dataTypeUpdated = typeof totalCount;
+console.log("Updated type value:",dataTypeUpdated);
+
+
+
+
+
+
+
+
+
+

8d8fb2ed2f5bba620623faedcf2850b8b9456de0
pruthvirajkamble <20bcs104@iiitdwd.ac.in>
2024-03-16 21:59:05 +0530

    Using termary operator and creating function for each step
    Using termary operator and creating function for each step

---
+++ 05_operatorB.js
@@ -0,0 +1,34 @@
+
+console.log("========================Step-1=========================");
+function greaterNumber(n1, n2){
+   
+   var num = n1>n2?n1:n2;
+   console.log(`The greatest number is: ${num}`);
+}
+greaterNumber(10,-10);
+greaterNumber(800,899);
+
+console.log("========================Step-2=========================");
+
+function isEvenOrOddNum(n1){
+    var val = n1%2==0?"True":"False";
+    console.log(`Finding the even and odd number: ${val}`);
+}
+isEvenOrOddNum(29);
+isEvenOrOddNum(44);
+isEvenOrOddNum(0);
+isEvenOrOddNum(101);
+
+console.log("========================Step-3=========================");
+function wordLength(num1){
+  var len = num1.length;
+  var result = len%2==0?"EVEN":"ODD";
+  console.log(`The word length is:${len} and its:${result}`);
+}
+wordLength("JavaScript");
+wordLength("developer");
+wordLength("Google");
+
+
+
+
---
+++ 05_postFix.js
@@ -0,0 +1,24 @@
+var a= 5;
+//var b =a++;
+//var c =++a;
+console.log(`value of a ${a}` );
+//console.log(`value of b ${b}` );
+//console.log(`value of c ${c}` );
+var res = a++ + ++a + a++;
+console.log(`value of res ${res}` );
+console.log("=========2nd Expression==========");
+var a =2;
+console.log(`value of a ${a}`);
+var b = ++a + ++a + a++ - --a;
+console.log(`value of b ${b}`);
+console.log(`value of a ${a}`);
+
+console.log("=========3nd Expression==========");
+var a = 4;
+console.log(`value of a ${a}`);
+var b =a-- + ++a + ++a + a++;
+console.log(`value of b ${b}`);
+console.log(`value of a ${a}`);
+
+console.log("=========4nd Expression==========");
+var n =10;

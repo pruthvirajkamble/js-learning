f1d3722850cccffb694ff57a168215dd21dafded
pruthvirajkamble <20bcs104@iiitdwd.ac.in>
2024-04-07 13:30:13 +0530

    count the space and counting the prime number in the array
    count the space and counting the prime number in the array

--- 06_countPrime.js
+++ 06_countPrime.js
@@ -1,9 +1,38 @@
-    const array = []
-    for (let index = 2; index < num; index++) {
-      if (num%index==0) {
-       return false;
-      } 
+    //const array = [3,9,7,6,19,29,53];
+    function isPrime(num) {
+      if (num <= 1) {
+          return false;
+      }
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+          if (num % i === 0) {
+              return false;
+          }
+      }
+      return true;
+  }
+  
+  function countPrimesInArray(arr) {
+      let count = 0;
+      for (let i = 0; i < arr.length; i++) {
+          if (isPrime(arr[i])) {
+              count++;
+          }
+      }
+      return count;
+  }
+  
+  const array = [3, 9, 7, 6, 19, 29, 53];
+  console.log("Number of prime numbers in the array:", countPrimesInArray(array));
+  
+  function spaceCount(sentence){
+   let count = 0;
+   for(let i = 0;i<sentence.length;i++){
+    if (sentence[i]===" "){
+      count++;
     }
-    return true;
-   
-   
+   }
+   return count;
+  }
+  console.log(`Total number of space:${spaceCount("Revision is the mother of success")}`);
+  console.log(`Total number of space:${spaceCount("java script is the language of internet world")}`);
+
